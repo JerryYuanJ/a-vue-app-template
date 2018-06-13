8ef8142d17dbf0c9d14f067b450db99bdff9267a
Jerry <610819267@qq.com>
2018-06-13 21:19:54 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import store from './store/index.js'
 import echarts from 'echarts'
 //引入font-awesome.css
 // import 'font-awesome/css/font-awesome.css'
-import 'font-awesome/css/font-awesome.min.css'
+// 暂时从cdn上获取,此issue以后有时间再研究
 Vue.prototype.$echarts = echarts;
 
 Vue.use(MintUI);
